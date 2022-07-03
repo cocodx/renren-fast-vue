be9ff043f02b14e973b2572ad8f5def1402cbdaf
刘刚 <2cevening@gmail.com>
2022-07-04 00:06:11 +0800

    feature(brand):表单自定义校验

--- src/views/modules/product/brand-add-or-update.vue
+++ src/views/modules/product/brand-add-or-update.vue
@@ -29,7 +29,7 @@
       <el-input v-model="dataForm.firstLetter" placeholder="检索首字母"></el-input>
     </el-form-item>
     <el-form-item label="排序" prop="sort">
-      <el-input v-model="dataForm.sort" placeholder="排序"></el-input>
+      <el-input v-model.number="dataForm.sort" placeholder="排序"></el-input>
     </el-form-item>
     </el-form>
     <span slot="footer" class="dialog-footer">
@@ -69,10 +69,26 @@
             { required: true, message: '显示状态', trigger: 'blur' }
           ],
           firstLetter: [
-            { required: true, message: '检索首字母不能为空', trigger: 'blur' }
+            { validator:(rule,value,callback)=>{
+              if(value == ''){
+                callback(new Error('首字母必须填写'))
+              }else if( !/^[a-zA-Z]$/.test(value) ){
+                callback(new Error('首字母必须a-z或者A-Z之间'))
+              }else{
+                callback()
+              }
+              }, trigger: 'blur' }
           ],
           sort: [
-            { required: true, message: '排序不能为空', trigger: 'blur' }
+            { validator:(rule,value,callback)=>{
+                if(value==''){
+                  callback(new Error('排序字段必须填写'))
+                }else if(!Number.isInteger(value) || value<0){
+                  callback(new Error('排序字段必须是一个整数且大于等于0'))
+                }else{
+                  callback()
+                }
+              }, trigger: 'blur' }
           ]
         }
       }
